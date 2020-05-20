25d9294f9b46e0cb9b24169522572390c693ccc4
alexkp98 <65607907+alexkp98@users.noreply.github.com>
2020-05-20 14:53:54 +0530

    set

---
+++ gallery/gallery.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('#get-data').click(function () {
+      var showData = $('#show-data');
+  
+      $.getJSON('gallery.json', function (data) {
+        console.log(data);
+  
+        var items = data.items.map(function (item) {
+          return item.key + ': ' + item.value;
+        });
+  
+        showData.empty();
+  
+        if (items.length) {
+          var content = '<li>' + items.join('</li><li>') + '</li>';
+          var list = $('<ul />').html(content);
+          showData.append(list);
+        }
+      });
+  
+      showData.text('Loading the JSON file.');
+    });
+  });
+
+  $('#get-data').click(function () {
+    $.getJSON('gallery.json', function (data) {
+      console.log(data);
+    });
+  });
