7cb22a8ab2bab8d90d6f416d7e1023bb70cad3ae
alexkp98 <65607907+alexkp98@users.noreply.github.com>
2020-05-23 09:51:44 +0530

    include

---
+++ collection.js
@@ -0,0 +1,20 @@
+// var json = require('./img.json');\
+
+
+var data = [{
+    "id": 1
+    
+  }, {
+    "id": 2
+    
+  }, {
+    "id": 3
+  }]
+
+
+function alert(){
+    console.log(data.length);
+    data.insert[4];
+    console.log(data.length);
+    // console.log(word);
+}
